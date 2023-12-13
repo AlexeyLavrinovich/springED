c34a3137a91290cd4cf90db72feee9b3602a9dc9
Alexey Lavrinovich <lavrinleha95@gmail.com>
2023-12-13 09:53:11 -0800

    Add config for basic authentication with no encrypt

--- src/main/java/com/aliakseila/springED/service/UserService.java
+++ src/main/java/com/aliakseila/springED/service/UserService.java
@@ -1,20 +1,38 @@
 package com.aliakseila.springED.service;
 
 import com.aliakseila.springED.entity.User;
+import com.aliakseila.springED.mapper.UserMapper;
+import com.aliakseila.springED.model.UserModel;
 import com.aliakseila.springED.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepo userRepo;
 
-    public List<User> getAllUsers() {
-        return userRepo.findAll();
+    public List<UserModel> getAllUsers() {
+        return userRepo.findAll().stream().map(UserMapper.INSTANCE::mapToModel).collect(Collectors.toList());
     }
 
+    public User findByUsername(String username) {
+        User user = userRepo.findByUsername(username);
+        if (user == null){
+            throw new UsernameNotFoundException(username);
+        }
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return findByUsername(username);
+    }
 }
