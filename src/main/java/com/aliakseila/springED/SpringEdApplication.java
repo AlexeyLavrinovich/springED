d3aaf2fb3ff834c6c74d0fdc2a3edd10aacf8a84
Alexey Lavrinovich <lavrinleha95@gmail.com>
2023-12-13 04:33:11 -0800

    init commit

---
+++ src/main/java/com/aliakseila/springED/SpringEdApplication.java
@@ -0,0 +1,13 @@
+package com.aliakseila.springED;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliakseila/springED/SpringEdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliakseila.springED;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
