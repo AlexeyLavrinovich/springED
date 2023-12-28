9662355e4b4da0430634beafc17884b51bb26920
Alexey Lavrinovich <lavrinleha95@gmail.com>
2023-12-28 01:22:53 -0800

    Add get mapping for Profile

--- src/main/java/com/aliakseila/springED/controller/ProfilePageController.java
+++ src/main/java/com/aliakseila/springED/controller/ProfilePageController.java
@@ -1,6 +1,9 @@
 package com.aliakseila.springED.controller;
 
+import com.aliakseila.springED.mapper.ProfileMapper;
+import com.aliakseila.springED.model.entity.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProfilePageController {
 
     @GetMapping
-    public ResponseEntity getProfile(){
-        return ResponseEntity.ok("");
+    public ResponseEntity getProfile(@AuthenticationPrincipal User user){
+        return ResponseEntity.ok(ProfileMapper.INSTANCE.mapToDto(user.getProfile()));
     }
 
 
